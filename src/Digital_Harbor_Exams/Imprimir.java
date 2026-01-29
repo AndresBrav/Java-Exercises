eacee217504088ac72c4c8f4d70e4b54dc8d57bb
Andres Bravo <bravoandres1706@gmail.com>
2026-01-29 13:52:18 -0400

    digital harbor ejercicio imprimir realizado

---
+++ src/Digital_Harbor_Exams/Imprimir.java
@@ -0,0 +1,16 @@
+package Digital_Harbor_Exams;
+
+public class Imprimir {
+    public static void main(String[] args) {
+        imprimir();
+    }
+
+    public static void imprimir() {
+        System.out.println("JJJJJJJ A V V A\n" +
+                " J A A V V A A\n" +
+                "J J A A V V A A\n" +
+                "J J AAAAAAA V V AAAAAAA\n" +
+                " J A A V A A");
+
+    }
+}

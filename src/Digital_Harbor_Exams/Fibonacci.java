c44b401c545415091255947f3f6fb9335507b7db
Andres Bravo <bravoandres1706@gmail.com>
2026-02-05 16:01:39 -0400

    Gato completo

--- src/Digital_Harbor_Exams/Fibonacci.java
+++ src/Digital_Harbor_Exams/Fibonacci.java
@@ -1,44 +1,41 @@
 package Digital_Harbor_Exams;
 
-import java.util.Scanner;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Fibonacci {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long a = sc.nextLong();
-        long b = sc.nextLong();
-        long N = sc.nextLong();
 
-        // System.out.println(a + " " + b + " " + N);
-        long resultado = calcularSucesion(a, b, N);
-        System.out.println("El resultado es " + resultado);
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+            long N = Long.parseLong(st.nextToken());
+
+            long resultado = calcularSucesion(a, b, N);
+            System.out.print(resultado);
+        } catch (Exception e) {
+        }
     }
 
     public static long calcularSucesion(long a, long b, long N) {
 
-        long iterador = 2;
-        long resultado = 0;
-        if (N == 1) {
+        if (N == 1)
             return a;
-        }
-        if (N == 2) {
+        if (N == 2)
             return b;
-        }
 
-        while (iterador < N) {
+        long resultado = 0;
+
+        for (long i = 3; i <= N; i++) {
             resultado = (b * b) + a;
             a = b;
             b = resultado;
-
-            iterador++;
         }
 
-        // System.out.println("el resultado es " + resultado);
         return resultado;
     }
 
--- src/Digital_Harbor_Exams/Gato.java
+++ src/Digital_Harbor_Exams/Gato.java
@@ -1,7 +1,136 @@
 package Digital_Harbor_Exams;
 
-public class Gato {
-    public static void main(String[] args) {
+import java.util.Scanner;
+import java.util.ArrayList;
 
+public class Gato {
+
+    static String linea1Orejas = " /\\/\\";
+
+    static String linea2Emocionado = "(*_*)";
+
+    static String line2Asustado = "(o_o)";
+
+    static String linea2Enojado = "(-_-)";
+
+    static String linea2Amoroso = "(>_<)";
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> emociones = new ArrayList<>();
+
+        int numero = sc.nextInt();
+        sc.nextLine(); // limpiamos el salto de linea
+
+        for (int index = 0; index < numero; index++) {
+            String linea1 = sc.nextLine();
+            String linea2 = sc.nextLine();
+
+            emociones.add(linea1);
+            emociones.add(linea2);
+        }
+        sc.close();
+
+        // System.out.print(salida);
+        int i = 0;
+        boolean verCara = false;
+        while (i < emociones.size()) {
+            String palabra = emociones.get(i);
+            if (i % 2 == 0) {
+                String orejas = LimpiarOrejas(palabra);
+                if (orejas.equals(linea1Orejas)) {
+                    verCara = true;
+                    i = i + 1;
+                } else {
+                    System.out.println("No identificado");
+                    i = i + 2;
+                }
+            } else {
+                if (i % 2 != 0 && verCara) {
+                    String cara = LimpiarRostro(palabra);
+                    if (cara.equals(line2Asustado)) {
+                        System.out.println("Asustado");
+                    } else if (cara.equals(linea2Emocionado)) {
+                        System.out.println("Emocionado");
+                    } else if (cara.equals(linea2Enojado)) {
+                        System.out.println("Enojado");
+                    } else if (cara.equals(linea2Amoroso)) {
+                        System.out.println("Amoroso");
+                    } else {
+                        System.out.println("No identificado");
+                    }
+
+                    i++;
+
+                }
+            }
+
+        }
+
+    }
+
+    public static String LimpiarOrejas(String palabra) {
+
+        int i = 0;
+        String nuevaPalabra = "";
+
+        while (i < palabra.length()) {
+            if (i == 0) {
+                if (palabra.charAt(i) == ' ') {
+                    nuevaPalabra = nuevaPalabra + palabra.charAt(i);
+                }
+                i++;
+            } else {
+                if (palabra.charAt(i) != ' ') {
+                    nuevaPalabra = nuevaPalabra + palabra.charAt(i);
+                    i++;
+                } else {
+                    if (palabra.charAt(i) == ' ' && palabra.charAt(i + 1) != ' ' && i < palabra.length()) {
+                        i++;
+                    } else {
+                        if (palabra.charAt(i) == ' ' && palabra.charAt(i + 1) == ' ' && i < palabra.length()) {
+                            return "";
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+
+        return nuevaPalabra;
+    }
+
+    public static String LimpiarRostro(String palabra) {
+        int i = 0;
+        String nuevaPalabra = "";
+
+        while (i < palabra.length()) {
+            if (i == 0) {
+                if (palabra.charAt(i) == '(') {
+                    nuevaPalabra = nuevaPalabra + palabra.charAt(i);
+                }
+                i++;
+            } else {
+                if (palabra.charAt(i) != ' ') {
+                    nuevaPalabra = nuevaPalabra + palabra.charAt(i);
+                    i++;
+                } else {
+                    if (palabra.charAt(i) == ' ' && palabra.charAt(i + 1) != ' ' && i < palabra.length()) {
+                        i++;
+                    } else {
+                        if (palabra.charAt(i) == ' ' && palabra.charAt(i + 1) == ' ' && i < palabra.length()) {
+                            return "";
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+        return nuevaPalabra;
     }
 }
